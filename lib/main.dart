0bd639d4781d5d8fd620b7168785309616e174f8
Mahmoud Omara <mahmoud.omara909@gmail.com>
2025-06-05 01:11:40 +0300

    [Feature] changed the title to itr 1 hello

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Iteration 1',
+      title: 'Iteration 1. Hello',
       theme: ThemeData(
         // This is the theme of your application.
         //
